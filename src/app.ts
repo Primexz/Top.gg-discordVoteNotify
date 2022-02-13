0417a6e39548fd103d0d9e7de5cef6ae46151023
Primexz <johann@pepebot.info>
2022-02-13 17:37:52 +0100

    add: base for discord webhook setup

--- src/app.ts
+++ src/app.ts
@@ -2,26 +2,35 @@ import express from 'express';
 import * as topGG from '@top-gg/sdk'
 import helmet from "helmet";
 import 'dotenv/config';
+import {constructEmbed} from "./modules";
+import fetch from 'node-fetch';
 
 //env variables
 const apiPort: string = process.env.WEB_PORT;
 const authSecret: string = process.env.WH_AUTHORIZATION;
 const tggRoute: string = process.env.WH_ROUTE;
+const discordWebhook = process.env.DC_WEBHOOK;
 
 //create express web-app
 const app = express();
 app.use(helmet());
 
-const tggHook = new topGG.Webhook(authSecret)
+const tggHook = new topGG.Webhook(authSecret);
 
-//route to receive topGG votes
+//POST route to receive topGG votes
 app.post(tggRoute, tggHook.listener(vote => {
-
+    fetch(discordWebhook, {
+        method: "POST",
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(constructEmbed(vote.bot, vote.user))
+    }).then(res => {
+        console.log(res);
+    })
 }))
 
 //start express
 app.listen(apiPort, () => {
     console.log(`Top-GG Vote Listenener was successfully started on port: ${apiPort}!`);
 })
-
-
---
+++ src/modules.ts
@@ -0,0 +1,5 @@
+export function constructEmbed(bot, voter) {
+    return {
+        "content":"test"
+    }
+}
