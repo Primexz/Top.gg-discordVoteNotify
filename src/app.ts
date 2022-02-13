896043fccb9d462e2644bc5cb09dde996e4ff645
Primexz <johann@pepebot.info>
2022-02-13 16:36:41 +0100

    add: helmet to express server

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
-import express from 'express'
-import topGG from '@top-gg/sdk'
-import 'dotenv/config'
+import express from 'express';
+import topGG from '@top-gg/sdk';
+import helmet from "helmet";
+import 'dotenv/config';
 
-const apiPort = process.env.WEB_PORT
+const apiPort = process.env.WEB_PORT;
 
 //create express web-app
-const app = express()
+const app = express();
+app.use(helmet());
 
 
 
 //start express
 app.listen(apiPort, () => {
-    console.log(`Top-GG Vote Listenener was successfully started on port: ${apiPort}!`)
+    console.log(`Top-GG Vote Listenener was successfully started on port: ${apiPort}!`);
 })
 
 
