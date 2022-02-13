965ec1b56dc114cc8b1fc9bf8d2b6269cfe49d6c
Primexz <johann@pepebot.info>
2022-02-13 16:51:47 +0100

    add: express route to receive votes

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,10 @@ import * as topGG from '@top-gg/sdk'
 import helmet from "helmet";
 import 'dotenv/config';
 
+//env variables
 const apiPort: string = process.env.WEB_PORT;
 const authSecret: string = process.env.WH_AUTHORIZATION;
+const tggRoute: string = process.env.WH_ROUTE;
 
 //create express web-app
 const app = express();
@@ -12,6 +14,10 @@ app.use(helmet());
 
 const tggHook = new topGG.Webhook(authSecret)
 
+//route to receive topGG votes
+app.post(tggRoute, tggHook.listener(vote => {
+
+}))
 
 //start express
 app.listen(apiPort, () => {
