3da4baff7c1f14a7b301bd2c3c0c44a52468d180
Primexz <johann@pepebot.info>
2022-02-13 17:46:35 +0100

    add: embed message template for webhook notify

--- src/modules.ts
+++ src/modules.ts
@@ -1,5 +1,16 @@
 export function constructEmbed(bot, voter) {
     return {
-        "content":"test"
+        content: `<@${bot.id}>`,
+        embeds: [
+            {
+                color: 11730954,
+                title: '<:topgg:918280202398875758> Top.gg | Discord Bot List',
+
+                description: 'description',
+
+                timestamp: new Date()
+            },
+        ],
+
     }
 }
