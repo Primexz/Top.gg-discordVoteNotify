b699d3b1b98166b764ccf85bd21a6fe2b0c66b9c
Primexz <johann@pepebot.info>
2022-02-13 19:05:39 +0100

    improved: embed layout

--- src/modules.ts
+++ src/modules.ts
@@ -7,7 +7,7 @@ export function constructEmbed(bot, voter: tggUser) {
             {
                 color: 11730954,
                 title: '<:topgg:918280202398875758> Top.gg | Discord Bot List',
-                description: `\`${voter.username}#${voter.discriminator}\` [voted](https://top.gg/bot/${bot}) bot: <@${bot}>`,
+                description: `\`${voter.username}#${voter.discriminator}\` [voted](https://top.gg/bot/${bot}) bot: <@${bot}>\n\n❤️❤️`,
                 thumbnail: {
                     url: `https://cdn.discordapp.com/avatars/${voter.id}/${voter.avatar}.webp`
                 },
