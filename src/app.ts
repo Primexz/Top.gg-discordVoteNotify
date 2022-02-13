8f173bcf95a52c4e867c313a704ca0cb35c60944
Primexz <johann@pepebot.info>
2022-02-13 16:45:19 +0100

    improve: typescript layout

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import express from 'express';
-import topGG from '@top-gg/sdk';
+import * as topGG from '@top-gg/sdk'
 import helmet from "helmet";
 import 'dotenv/config';
 
-const apiPort = process.env.WEB_PORT;
+const apiPort: string = process.env.WEB_PORT;
+const authSecret: string = process.env.WH_AUTHORIZATION;
 
 //create express web-app
 const app = express();
 app.use(helmet());
 
+const tggHook = new topGG.Webhook(authSecret)
 
 
 //start express
